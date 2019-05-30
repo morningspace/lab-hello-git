83cac41147008122d692712a2ef7d2ca5293fb7a
morningspace <morningspace@yahoo.com>
2019-05-30 21:03:14 +0800

    Custmize user account and add more tools

--- docker/git-local/entrypoint.sh
+++ docker/git-local/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# sample git config 
+user_name=${user_name:-"dev"}
+user_email=${user_email:-"dev@example.com"}
+
+git config --global user.name "$user_name" && \
+git config --global user.email "$user_email" && \
+ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -C "$user_email" -N ""
+
 cat WELCOME
 cat ~/.ssh/id_rsa.pub
 
